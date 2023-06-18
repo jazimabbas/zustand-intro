75f67237ea6a012cddd190c52161d2ed089693c6
Jazim Abbas <jazimabbas87@gmail.com>
2023-06-18 16:31:35 +0500

    Added basic counter example with zuztand

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,22 @@
+import useCounterStore from "./store";
+
 export default function Homepage() {
+  const counter = useCounterStore((state) => state.counter);
+  const handleIncrement = useCounterStore((state) => state.increment);
+  const handleDecrement = useCounterStore((state) => state.decrement);
+  const handleReset = useCounterStore((state) => state.reset);
+
   return (
     <>
       <h2 className="center">Counter</h2>
+      <div>
+        <p className="bold center">Counter Value: {counter}</p>
+        <div className="counter-actions">
+          <button onClick={handleIncrement}>Increment</button>
+          <button onClick={handleDecrement}>Decrement</button>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      </div>
     </>
   );
 }
